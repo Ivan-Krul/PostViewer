306d41a4ebe40ba1cdad3d123eb19aa5fb96b936
Ivan-Krul <kiwiivanya128@gmail.com>
2024-12-13 00:22:12 +0100

    added css file to inherit style, changed style for images and trying to add plain text tag

--- post_parser.js
+++ post_parser.js
@@ -105,6 +105,9 @@ export const dictionary =
     [">q> ", "<blockquote style=\"color: var(--semi);\">"],
     [" >q>", "</blockquote>"],
 
+    ["|S- ", "<style>"],
+    [" -S|", "</style>"],
+
     ["|$- ", `<script type=\"module\" src=\"`],
     [" -$|", "\"></script>"],
 
@@ -114,9 +117,84 @@ export const dictionary =
     ["!|", "<br/>"]
   ];
 
+export var ignoreList = [];
+
+export const ignoreParsingOpen = "##>";
+export const ignoreParsingClose = "<##";
+
+function cropIgnored(str = "") {
+  ignoreList = [];
+  
+  let indxOp = str.indexOf(ignoreParsingOpen, 0);
+  let indxCl = str.indexOf(ignoreParsingClose, 0);
+  
+  let cleanStr = "";
+  if(indxOp === -1)
+    cleanStr = str;
+  else
+    cleanStr = str.substr(0, indxOp + ignoreParsingOpen.length);
+  
+  let breakpointInd = 0;
+  
+  while(indxOp !== -1 && breakpointInd < 100) {
+    breakpointInd++;
+    if(indxCl === -1) {
+      ignoreList.push(str.substr(indxOp + ignoreParsingOpen.length));
+      break;
+    }
+    else
+      ignoreList.push(str.substr(indxOp + ignoreParsingOpen.length, indxCl - (indxOp + ignoreParsingOpen.length)));
+    
+    indxOp = str.indexOf(ignoreParsingOpen, indxCl + ignoreParsingClose.length);
+    
+    if(indxOp === -1) {
+      cleanStr += str.substr(indxOp + ignoreParsingClose.length);
+    }
+    else
+      cleanStr += str.substr(indxCl + ignoreParsingClose.length, indxOp + ignoreParsingOpen.length);
+    
+    indxCl = str.indexOf(ignoreParsingClose, indxOp + ignoreParsingOpen.length);
+  }
+  
+  return cleanStr;
+}
+
+function revertIgnored(cleanStr = "") {
+  let indxOp = cleanStr.indexOf(ignoreParsingOpen, 0);
+  let addIndxOp = cleanStr.indexOf(ignoreParsingOpen, indxOp + ignoreParsingOpen.length);
+  
+  let str = "";
+  if(indxOp === -1)
+    str = cleanStr;
+  else
+    str = cleanStr.substr(0, indxOp);
+  
+  let i = 0;
+  let breakpointInd = 0;
+  
+  while(indxOp !== -1 && breakpointInd < 100) {
+    breakpointInd++;
+    str += ignoreList[i];
+    i++;
+    
+    if(addIndxOp === -1) {
+      str += cleanStr.substr(indxOp + ignoreParsingOpen.length);
+      break;
+    }
+    else {
+      str += cleanStr.substr(indxOp + ignoreParsingOpen.length, addIndxOp - (indxOp + ignoreParsingOpen.length));
+      indxOp = addIndxOp;
+      addIndxOp = cleanStr.indexOf(ignoreParsingOpen, indxOp + ignoreParsingOpen.length);
+    }
+  }
+  
+  return str;
+}
+
 function adaptRawString(str = "", forcedLang = undefined) {
   let strCom = str.replaceAll("\r", "");
-  return getLangTag(strCom, forcedLang);
+  let langCom = getLangTag(strCom, forcedLang);
+  return cropIgnored(langCom);
 }
 
 export function parseRawTitle(str = "", forcedLang = undefined) {
@@ -135,7 +213,10 @@ export function parseRawPost(str = "", forcedLang = undefined) {
   dictionary.forEach((regex_value) => {
     res = replaceAllOccurrences(res, regex_value[0], regex_value[1]);
   });
-  return res;
+  
+  //console.log(ignoreList);
+ 
+  return revertIgnored(res);
 }
 
 export function appendParsedPostToDOM(parsedContent) {
